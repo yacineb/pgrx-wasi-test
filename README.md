18afccb5d16d891641227abdfa9739222cbaabca
yacineb <ybsoft2011@live.com>
2024-12-02 18:07:27 +0100

    build: script for building ext on wasip1

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-##  Get the wasi sdk modified version
+# pgrx on wasi POC
+
+## Get the wasi sdk modified version
 
 - Option 1: `git clone https://github.com/pygame-web/python-wasi-sdk.git` then run `./python-wasi-sdk.sh` to build from source (see readme of pglite-build)
 
 - Option 2 (Ubuntu am64 only): run `./wasi-sdk.sh` which directly downloads the compiled sdk.
 
-##  Building pglite
+## Building pglite
 
-- clone https://github.com/electric-sql/pglite-build.git
+- `git clone https://github.com/electric-sql/pglite-build.git`
 - export SDKROOT=${SDKROOT:-/opt/python-wasm-sdk}
 - ./wasisdk.sh
 
-## Build the extension:
+## Build the extension
+
 ```sh
 ./build.sh
 ```
-
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-
+SDKROOT=/tmp/python-wasm-sdk
 
 export WASI_SDK_PATH=/opt/python-wasm-sdk/wasisdk/upstream
 
@@ -6,15 +6,30 @@ echo '/opt/python-wasm-sdk/wasisdk/bin/wasi-run /tmp/pglite/bin/pg_config "$@"' 
 
 export PATH="/tmp/pglite/bin/:/opt/python-wasm-sdk/wasisdk/bin:$PATH"
 
-export BINDGEN_EXTRA_CLANG_ARGS_wasm32_wasip1="-isystem /opt/python-wasm-sdk/wasisdk/upstream/share/wasi-sysroot/include"
+export BINDGEN_EXTRA_CLANG_ARGS_wasm32_wasip1="-isystem $WASI_SDK_PATH/share/wasi-sysroot/include"
 # export RUSTFLAGS="--sysroot $WASI_SDK_PATH/share/wasi-sysroot"
 # export CC=$WASI_SDK_PATH/bin/clang 
 # export CXX=$WASI_SDK_PATH/bin/clang++
 # export CXXFLAGS="-fno-exceptions"
 # export PGRX_BINDGEN_NO_DETECT_INCLUDES=1
 
-. /opt/python-wasm-sdk/wasm32-wasi-shell.sh
+. $SDKROOT/wasm32-wasi-shell.sh
 
-# install rust sdk
-/opt/python-wasm-sdk/lang/rustsdk.sh
-cargo +nightly build  --target wasm32-wasip2 --lib --features pg16,pgrx/cshim --no-default-features
+echo "set up rust toolchain"
+pushd ${SDKROOT}
+    . ${CONFIG:-config}
+    . wasm32-bi-emscripten-shell.sh
+popd
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > getrust
+bash ./getrust -y -t wasm32-unknown-unknown --default-toolchain nightly
+
+. $SDKROOT/rust/env
+
+
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-unknown-emscripten
+rustup target add wasm32-wasip1
+
+echo building the extension
+cargo +nightly build  --target wasm32-wasip1 --lib --features pg16,pgrx/cshim --no-default-features
