c57102c8f53ab8ff05d699f6f8952bc6d6f7c7d9
yacineb <ybsoft2011@live.com>
2024-12-04 15:10:21 +0100

    build: ext build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SDKROOT=/opt/python-wasm-sdk
+export SDKROOT=/opt/python-wasm-sdk
 
 export WASI_SDK_PATH="$SDKROOT/wasisdk/upstream"
 
@@ -13,7 +13,7 @@ export PGRX_PG_CONFIG_PATH=/tmp/pglite/bin/pg_config_wrapper
 export PGRX_BINDGEN_NO_DETECT_INCLUDES=1
 
 export BINDGEN_EXTRA_CLANG_ARGS_wasm32_wasip1="-isystem $WASI_SDK_PATH/share/wasi-sysroot/include"
-# export RUSTFLAGS="--sysroot $WASI_SDK_PATH/share/wasi-sysroot"
+export RUSTFLAGS="--sysroot $WASI_SDK_PATH/share/wasi-sysroot"
 # export CC=$WASI_SDK_PATH/bin/clang 
 # export CXX=$WASI_SDK_PATH/bin/clang++
 # export CXXFLAGS="-fno-exceptions"
@@ -24,7 +24,6 @@ export BINDGEN_EXTRA_CLANG_ARGS_wasm32_wasip1="-isystem $WASI_SDK_PATH/share/was
 echo "building the extension pgrx-wasi/pgrx-examples/$1"
 pushd pgrx-wasi/pgrx-examples/$1
 
-cargo +nightly build --target wasm32-wasip1 --sysroot "$WASI_SDK_PATH/share/wasi-sysroot" \
-           --lib --features pg16,pgrx/cshim --no-default-features
+cargo +nightly build -Zbuild-std --verbose --target wasm32-wasip1 --lib --features pg16,pgrx/cshim --no-default-features
 
 popd
