f9c1cee12549478cb85ed5ccce1322fa52c02e1d
yacineb <ybsoft2011@live.com>
2024-12-06 08:57:10 +0100

    build: pass  relocation model as pic to rustc

--- build.sh
+++ build.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# rustc codegen options: https://doc.rust-lang.org/rustc/codegen-options/index.html
+
 export SDKROOT=/opt/python-wasm-sdk
 
 export WASI_SDK_PATH="$SDKROOT/wasisdk/upstream"
+export WASI_SYSROOT="$WASI_SDK_PATH/share/wasi-sysroot"
+
+# bindgen and rustc flags
+export BINDGEN_EXTRA_CLANG_ARGS_WASM32_WASIP1="-isystem $WASI_SYSROOT/include"
+export CARGO_TARGET_WASM32_WASIP1_LINKER="$WASI_SDK_PATH/bin/clang"
+
+# relocation-model fully relocatable position independent code, machine instructions need to use relative addressing modes.
+# Equivalent to the "uppercase" -fPIC or -fPIE options in other compilers, depending on the produced crate types.
+# pass the sysroot down to the linker
+export CARGO_TARGET_WASM32_WASIP1_RUSTFLAGS="-Crelocation-model=pic -Clink-arg=--sysroot=$WASI_SYSROOT"
+
+#export CC="$WASI_SDK_PATH/bin/clang"
+#export CXX="$WASI_SDK_PATH/bin/clang++"
+# export LIBCLANG_PATH=/path/to/custom/libclang
+
+. $SDKROOT/wasm32-wasi-shell.sh
+. $SDKROOT/${CONFIG:-config}
+
 
 # set pg_config (wrapper) path
 echo '/opt/python-wasm-sdk/wasisdk/bin/wasi-run /tmp/pglite/bin/pg_config.wasi "$@"' > /tmp/pglite/bin/pg_config
@@ -12,20 +32,8 @@ export PATH="/tmp/pglite/bin:$PATH"
 export PGRX_PG_CONFIG_PATH=/tmp/pglite/bin/pg_config_wrapper
 export PGRX_BINDGEN_NO_DETECT_INCLUDES=1
 
-# bindgen and rustc flags
-export BINDGEN_EXTRA_CLANG_ARGS_wasm32_wasip1="-isystem $WASI_SDK_PATH/share/wasi-sysroot/include"
-export RUSTFLAGS="--sysroot $WASI_SDK_PATH/share/wasi-sysroot"
-
-#export CC=$WASI_SDK_PATH/bin/clang 
-#export CXX=$WASI_SDK_PATH/bin/clang++
-# export CXXFLAGS="-fno-exceptions"
-
-. $SDKROOT/wasm32-wasi-shell.sh
-. $SDKROOT/${CONFIG:-config}
-
 echo "building the extension pgrx-wasi/pgrx-examples/$1"
 pushd pgrx-wasi/pgrx-examples/$1
 
-cargo +nightly build -Z build-std=core,alloc --verbose --target wasm32-wasip1 --lib --features pg16,pgrx/cshim --no-default-features
-
+cargo +nightly build -Z build-std=core,alloc --verbose --target wasm32-wasip1 --lib
 popd
